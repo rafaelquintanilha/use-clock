755dc880de1814d6e69c2a3ea5ab68a6feceb2d2
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2019-02-21 22:57:22 -0500

    Updates

---
+++ precache-manifest.bcff274b36da2c49e8f19262d3ef26f0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "08ca6318712b09436cc6",
+    "url": "/static/js/main.08ca6318.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0a6f61f182a163ac5299",
+    "url": "/static/js/2.0a6f61f1.chunk.js"
+  },
+  {
+    "revision": "4155d34b8de9452c65caadc1f37fb70e",
+    "url": "/index.html"
+  }
+];
